f36ac1176a781fe04aed52e11955e2d981aa9f18
Scott Stevenson <scotifer92@googlemail.com>
2018-06-25 08:06:29 +0100

    Fixed git issue. Adds missing integration tests and unit test for NaN dividing by zero,

---
+++ js_calculator_start_point/tests/integration/tests.js
@@ -0,0 +1,82 @@
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
+describe('calculator functionality', function() {
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+    browser.get('http://localhost:3000');
+  });
+
+  // write integration tests here in the form of "it should do something..."
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  it('should add 1 + 4 and get 5', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5')
+})
+
+  it('should subtract 7 -4 and return 3', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number7')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3')
+  })
+
+  it('should multiply 3 and 5 and return 15', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('15')
+  })
+
+  it('should concatenate multiple number button clicks and return the correct number', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('144')
+  })
+
+  it('should chain operators', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number7')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+
+    expect(running_total.getAttribute('value')).to.eventually.equal('14')
+  })
+
+  it('should be show NaN when you try to divide by zero', function(){
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+
+    expect(running_total.getAttribute('value')).to.eventually.equal("NaN");
+  })
+
+
+
+
+});
---
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -0,0 +1,60 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('has a sample test', function(){
+    assert.equal(true, true)
+  })
+
+  it('can add numbers together', function(){
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    assert.equal(calculator.runningTotal, 5);
+  })
+
+  it('can subtract numbers', function() {
+    calculator.previousTotal = 7;
+    calculator.subtract(4)
+    assert.equal(calculator.runningTotal, 3)
+  })
+
+  it('can multiply numbers together', function() {
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    assert.equal(calculator.runningTotal, 15)
+  })
+
+  it('can divide numbers', function() {
+    calculator.previousTotal = 21
+    calculator.divide(7)
+    assert.equal(calculator.runningTotal, 3)
+  })
+
+  xit('can recognise a click on number button', function() {
+
+
+  })
+
+  xit('can click mathematical operator buttons', function() {
+
+  })
+
+  xit('can clear running total on clicking clear button', function() {
+
+  })
+
+  it('should return NaN when dividing by zero', function() {
+    calculator.numberClick(1);
+    calculator.numberClick(0);
+    calculator.operatorClick('/');
+    calculator.numberClick(0);
+    calculator.operatorClick("=");
+    assert.strictEqual(calculator.runningTotal, "NaN");
+  })
+
+});
